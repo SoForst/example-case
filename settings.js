ac8bb2d528bf230930f905c9949a0b84977f33b3
SoForst <96701479+SoForst@users.noreply.github.com>
2023-04-08 15:42:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.namabot = "Zenno Bot"
+global.namaowner = "Zenno"
+global.packname = "Zenno Bot"
+global.author = "Zenno"
+global.myweb = "....."
+global.footer_text = "© " + "Zenno"
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6285932251730']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
